13ba8c9148bc3361227cd81fbea33660f1db9794
Michael Owuor <michael.owuor@gmail.com>
2025-02-27 11:17:57 +0300

    chore: initial commit

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,19 +1,47 @@
-import React from 'react'
-import './styles.css'
+import { ReactNode } from "react";
+import { Comfortaa as FontHeader, Montserrat as FontBody } from "next/font/google";
 
-export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
-}
+import { cn } from "@/utils";
 
-export default async function RootLayout(props: { children: React.ReactNode }) {
-  const { children } = props
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
-  return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
-    </html>
-  )
-}
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+
+import type { Metadata } from "next";
+
+import "@/styles/globals.css";
+
+export const metadata: Metadata = {
+	title: "NextJS Starter Template",
+	description: "Template to get started with Next.js, Tailwind CSS and shadcn/ui.",
+};
+
+const fontHeader = FontHeader({ subsets: ["latin"], variable: "--font-header" });
+const fontBody = FontBody({ subsets: ["latin"], variable: "--font-body" });
+
+const RootLayout = async ({ children }: Readonly<{ children: ReactNode }>) => {
+	return (
+		<html lang="en" suppressHydrationWarning className={cn(fontHeader.variable, fontBody.variable)}>
+			<head>
+				<link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+			</head>
+
+			<body className="font-body flex h-screen flex-col">
+				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+					<header>
+						<Header />
+					</header>
+
+					<main>{children}</main>
+
+					<footer className="mt-auto">
+						<Footer />
+					</footer>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+};
+
+export default RootLayout;
